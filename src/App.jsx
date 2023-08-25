5ef7693ba840d4e62fe9a9509d6e685f2d8a5136
LouisWackenier <Louis.wackenier@gmail.com>
2023-08-25 20:34:40 +0200

    Built the Navbar but there is a bug

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,21 @@ import React from 'react';
 import GlobalStyle from './globalStyles'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
+//COMPONENTS 
+import Navbar from './components/Navbar/Navbar';
+
 //PAGES 
 import Home from './pages/Home';
 import SignUp from './pages/SignupPage';
 import Pricing from './pages/PricingPage'
 
+
+
 function App() {
   return  (
    <Router>
      <GlobalStyle />
-     
+     <Navbar />
      <Routes>
        <Route path='/' exact Component={Home} />
        <Route path='/SignUp' exact Component={SignUp} />
